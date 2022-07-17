dfcf8bbeabec3ad7f104d0938d988c80541c1a22
shivani agarwal <shiviagarwal6161@gmail.com>
2022-07-17 22:02:15 +0530

    Add changes

--- src/App.js
+++ src/App.js
@@ -7,11 +7,7 @@ import {
   Route,
 } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar';
-import Menu from './Components/Menu/Menu';
-import About from './Components/About/About';
-import Gallery from './Components/Gallery/Gallery';
-import Contact from './Components/Contact/Contact';
-import BookATable from './Components/BookTable/BookATable';
+
 
 function App() {
   return (
@@ -20,11 +16,6 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Discover />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/book-a-table" element={<BookATable />} />
         </Routes>
         <Footer />
       </BrowserRouter>
